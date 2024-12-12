fdb2e7a1ab883698e9ede62262256fbec1eccc08
kolane <1784210334@qq.com>
2024-12-12 13:20:48 +0800

    udp demo 修改成tokio异步

--- udp_demo/src/main.rs
+++ udp_demo/src/main.rs
@@ -1,6 +1,6 @@
-use std::net::{SocketAddr, UdpSocket};
-use std::sync::{mpsc::{self, Sender, Receiver}, Arc};
-use std::thread;
+use tokio::net::UdpSocket;
+use tokio::sync::mpsc;
+use std::sync::Arc;
 use actix_web::{web, App, HttpServer, Responder};
 
 pub struct UdpServer {
@@ -9,68 +9,60 @@ pub struct UdpServer {
 
 impl UdpServer {
     // 创建新的 UDP 回声服务器
-    pub fn new(bind_addr: &str) -> std::io::Result<Self> {
-        let socket = Arc::new(UdpSocket::bind(bind_addr)?);
+    pub async fn new(bind_addr: &str) -> std::io::Result<Self> {
+        let socket = Arc::new(UdpSocket::bind(bind_addr).await?);
         Ok(Self { socket })
     }
 
     // 启动服务器
-    pub fn run(self) {
+    pub async fn run(self) {
         let socket_clone = self.socket.clone();
-        let (tx, rx) = mpsc::channel();
+        let (tx, rx) = mpsc::channel::<(String, std::net::SocketAddr)>(100); // 使用带缓冲的通道
 
-        // 启动生产者线程
-        self.start_producer(socket_clone.clone(), tx);
+        // 启动生产者任务
+        let producer = self.start_producer(socket_clone.clone(), tx);
+        // 启动消费者任务
+        let consumer = self.start_consumer(socket_clone.clone(), rx);
 
-        // 启动消费者线程
-        self.start_consumer(socket_clone.clone(), rx);
+        // 同时运行生产者和消费者
+        tokio::join!(producer, consumer);
     }
 
-    // 生产者线程：监听UDP消息并发送到通道
-    fn start_producer(&self, socket: Arc<UdpSocket>, tx: Sender<(String, SocketAddr)>) {
-        thread::spawn({
-            let socket = socket.clone();
-            let tx = tx.clone();
-            move || {
-                let mut buf = [0; 1024];
-                loop {
-                    match socket.recv_from(&mut buf) {
-                        Ok((size, src_addr)) => {
-                            let message = String::from_utf8_lossy(&buf[..size]).to_string();
-                            if let Err(e) = tx.send((message, src_addr)) {
-                                eprintln!("Failed to send message to channel: {}", e);
-                            }
-                        }
-                        Err(e) => {
-                            eprintln!("Failed to receive data: {}", e);
-                        }
+    // 生产者任务：监听UDP消息并发送到通道
+    async fn start_producer(
+        &self,
+        socket: Arc<UdpSocket>,
+        tx: mpsc::Sender<(String, std::net::SocketAddr)>,
+    ) {
+        let mut buf = [0; 1024];
+        loop {
+            match socket.recv_from(&mut buf).await {
+                Ok((size, src_addr)) => {
+                    let message = String::from_utf8_lossy(&buf[..size]).to_string();
+                    if let Err(e) = tx.send((message, src_addr)).await {
+                        eprintln!("Failed to send message to channel: {}", e);
+                        break;
                     }
                 }
+                Err(e) => {
+                    eprintln!("Failed to receive data: {}", e);
+                }
             }
-        });
+        }
     }
 
-    // 消费者线程：从通道接收消息并通过UDP发送回客户端
-    fn start_consumer(&self, socket: Arc<UdpSocket>, rx: Receiver<(String, SocketAddr)>) {
-        thread::spawn({
-            let socket = socket.clone();
-            move || {
-                loop {
-                    match rx.recv() {
-                        Ok((message, src_addr)) => {
-                            println!("Received and sending back: {}", message);
-                            if let Err(e) = socket.send_to(message.as_bytes(), src_addr) {
-                                eprintln!("Failed to send message: {}", e);
-                            }
-                        }
-                        Err(e) => {
-                            eprintln!("Receiver error: {}", e);
-                            break;
-                        }
-                    }
-                }
+    // 消费者任务：从通道接收消息并通过UDP发送回客户端
+    async fn start_consumer(
+        &self,
+        socket: Arc<UdpSocket>,
+        mut rx: mpsc::Receiver<(String, std::net::SocketAddr)>,
+    ) {
+        while let Some((message, src_addr)) = rx.recv().await {
+            println!("Received and sending back: {}", message);
+            if let Err(e) = socket.send_to(message.as_bytes(), src_addr).await {
+                eprintln!("Failed to send message: {}", e);
             }
-        });
+        }
     }
 }
 
@@ -83,9 +75,13 @@ async fn main() -> std::io::Result<()> {
     env_logger::init();
 
     // Start UDP server
-    let udp_server = UdpServer::new("0.0.0.0:12345").expect("Could not start UDP server");
+    let udp_server = UdpServer::new("0.0.0.0:12345").await.expect("Could not start UDP server");
     println!("UDP server is running on 0.0.0.0:12345");
-    udp_server.run();
+    
+    // 使用 tokio spawn 来运行 UDP 服务器
+    tokio::spawn(async move {
+        udp_server.run().await;
+    });
 
     // Start actix-web server
     println!("Starting actix-web server on http://127.0.0.1:8080");
